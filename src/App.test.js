142e14368dc0708c9f9e2b916e856153deea458a
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-02-01 03:51:36 +0530

    Add. A5-T9-Handling HTTP Errors - Without Solution

--- src/App.test.js
+++ src/App.test.js
@@ -3,42 +3,25 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 
-test("App component handles loading and data states as expected", async () => {
+test("App component renders error message when request fails", async () => {
   render(<App />);
 
   // Check if the "Fetch Movies" button is rendered
   const fetchMoviesButton = screen.getByText("Fetch Movies");
   expect(fetchMoviesButton).toBeInTheDocument();
 
-  // Mock the fetch function to return a resolved Promise with dummy data
-  global.fetch = jest.fn().mockResolvedValue({
-    json: jest.fn().mockResolvedValue({
-      results: [
-        {
-          episode_id: 1,
-          title: "A New Hope",
-          release_date: "1977-05-25",
-          opening_crawl: "A long time ago in a galaxy far, far away...",
-        },
-      ],
-    }),
-  });
+  // Mock the fetch function to return a rejected Promise with an error message
+  global.fetch = jest.fn().mockRejectedValue(new Error("Test error"));
 
   // Simulate a click on the "Fetch Movies" button
   fireEvent.click(fetchMoviesButton);
 
-  // Check if loading message is displayed
-  const loadingMessage = screen.getByText("Loading...");
-  expect(loadingMessage).toBeInTheDocument();
+  // Check if error message is displayed
+  const errorMessage = await screen.findByText("Test error");
+  expect(errorMessage).toBeInTheDocument();
 
-  // Wait for the async operation to complete
-  await waitFor(() => {
-    // Check if the MoviesList component is rendered with the fetched movie
-    const movieTitleElement = screen.getByText("A New Hope");
-    expect(movieTitleElement).toBeInTheDocument();
-  });
-
-  // Check if loading message is not displayed after fetching movies
+  // Check if loading message is not displayed after encountering an error
+  const loadingMessage = screen.queryByText("Loading...");
   expect(loadingMessage).not.toBeInTheDocument();
 
   // Restore the original fetch function
