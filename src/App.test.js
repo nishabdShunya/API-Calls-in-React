81d329abffe124e6481f5c4055852c8771fb2d4e
Nishant Choubisa <nishantchoubisa92@gmail.com>
2024-02-04 15:23:06 +0530

    Add. A5-T12-Deleting Movies from Database - Without Solution

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,8 @@
 import fs from "fs";
 
-test("Click on Add Movie button adds a new movie to the database and to the screen", () => {
+test("Click on cross button removes a movie from the database and the screen", () => {
   const appJsContent = fs.readFileSync("./src/App.js", "utf8");
-
-  // 1. Check for "JSON.stringify" at least once
-  expect(appJsContent).toMatch(/JSON\.stringify/g);
-
-  // 2. Check for "firebaseio" at least twice
   const firebaseioMatches = appJsContent.match(/firebaseio/g);
-  expect(firebaseioMatches && firebaseioMatches.length).toBeGreaterThanOrEqual(
-    2
-  );
-
-  // 3. Check for "POST" at least once
-  expect(appJsContent).toMatch(/POST/g);
+  expect(firebaseioMatches && firebaseioMatches.length).toBe(3);
+  expect(appJsContent).toMatch(/DELETE/g);
 });
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,9 +5,14 @@ import "./Movie.css";
 const Movie = (props) => {
   return (
     <li className="movie">
-      <h2>{props.title}</h2>
-      <h3>{props.releaseDate}</h3>
-      <p>{props.openingText}</p>
+      <div>
+        <button className="delete-btn">&times;</button>
+      </div>
+      <div>
+        <h2>{props.title}</h2>
+        <h3>{props.releaseDate}</h3>
+        <p>{props.openingText}</p>
+      </div>
     </li>
   );
 };
